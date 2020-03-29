4c412c941134de78320616b79f46b4832bd610bb
KR3KZ <ikreekzi@gmail.com>
2020-03-29 01:40:21 +0100

    Prettier log

--- DiscordBot/index.js
+++ DiscordBot/index.js
@@ -15,13 +15,14 @@ client.on("message", msg => {
             const channel = client.channels.cache.find(x => x.name == channelName)
             if (channel && channel.parentID != '649672006177914910') {
                 try {
-                    //client.channels.cache.get(channel.id).send('Une mise à jour du core est disponible !', { files: [coreFolder + fileName] })
-                    console.log(`${fileName} sent in ${channelName}`)
+                    client.channels.cache.get(channel.id).send('Une mise à jour du core est disponible !', { files: [coreFolder + fileName] })
+                    console.log(`-   ${fileName} sent in ${channelName}`)
                 } catch ($err) {
                     console.log(`Can't send ${fileName} in ${channelName}`)
                 }
             }
         });
+        console.log(`OK.`)
     }
 })
 client.login("NjkzNTM2MTAyNDY3ODk1NDI4.Xn-hng.M8TMvwjb_wvs1b_bWydBcTWq-fw")
